42fa478bae79f036e63e3a0ea8a0f288ef0f4ad5
Eddibfatima <119509000+Eddibfatima@users.noreply.github.com>
2023-03-08 19:45:46 +0100

    Add files via upload

---
+++ streamlit portfolio/app.py
@@ -0,0 +1,60 @@
+import streamlit as st
+
+# --- GENERAL SETTINGS ---
+st.title("My Portfolio")
+
+# Add a subheader
+st.subheader("About Me")
+st.write("Hi there! My name is fatima ezzahra and I am a web multimedia student and science of physical matter student.with a strong passion for the entire world of IT, particularly computer vision. I believe that by specializing in this area, I can make a meaningful impact and make people's lives easier through the use of innovative technologies. I am excited to continue my journey and make a difference in this exciting field")
+st.subheader("Education")
+
+st.write(" Science of physical matter student at the third year university ,I have a baccalaureate in physics branch from institution sidi elhadj elhabib. ")
+st.subheader("Contact Me")
+st.write("Feel free to reach out to me if you have any questions or if you just want to connect. You can find me on:")
+st.write("- LinkedIn: Fatima Ezzahra EDDIB")
+st.write("- GitHub: Eddibfatima")
+st.write("- Email: eddibfatimaezzahra9@gmail.com")
+
+# My portfolio content here
+
+    
+
+st.markdown("""
+## Technical Skills
+- design
+- physics science
+- web developement
+
+## Soft Skills
+- Communication
+- Teamwork
+- Adaptability
+- problem solving
+- motivation
+- business
+- social skills
+""")
+# Add the rest of my portfolio Streamlit 
+    
+st.subheader(
+        '''
+        Languages:
+        * Arabic
+        * French
+        * English
+        * Turk
+        '''
+)
+        
+   
+st.write(
+        '''
+        ## My interests are:
+        * Design
+        * Computer Vision
+        * Natural Language Processing
+        * RESTFul API development Python+Falsk and NodeJS+Express
+        * Learn other programming languages
+        '''
+)
+ 
